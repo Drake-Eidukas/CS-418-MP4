5102c2a69587798631e841d8598d984ef9667b03
Drake Eidukas <eidukas2@illinois.edu>
2017-12-08 15:53:11 -0600

    Add properties

--- Sphere.js
+++ Sphere.js
@@ -5,15 +5,47 @@
  * @class Sphere
  */
 class Sphere {
-  constructor (radius, mass, color, position, velocity) {
+  constructor (radius = 1, speed = 0.05) {
     this.radius = radius
-    this.mass = mass
-    this.color = color
-    this.position = position
-    this.velocity = velocity
+    this.speed = speed
+
+    this.position = vec3.create()
+    vec3.random(this.position)
+
+    this.velocity = vec3.create()
+    vec3.random(this.velocity, speed)
   }
 
   get radius () {
+    return this.radius
+  }
+
+  set radius (radius) {
+    this.radius = radius
+  }
+
+  get velocity () {
+    return this.velocity
+  }
+
+  set velocity () {
+    this.velocity = velocity
+    this.speed = vec3.length(this.velocity)
+  }
+
+  get speed () {
+    return this.speed 
+  }
+
+  set speed (speed) {
+    this.speed = Math.max(speed, -speed)
+  }
+
+  get color () {
+
+  }
+
+  get mass () {
     
   }
 }
