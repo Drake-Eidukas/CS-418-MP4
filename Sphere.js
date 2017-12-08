7b785c66ae23cf61b26adfc8cabbe6f8cf4e55ed
Drake Eidukas <eidukas2@illinois.edu>
2017-12-08 13:30:00 -0600

    Start MP

---
+++ Sphere.js
@@ -0,0 +1,19 @@
+/**
+ * A sphere represents the vertex mesh of a sphere.
+ * We will create a single model of a vertex mesh of a sphere within a 1 x 1 x 1 cube
+ * then scale and translate this model to be used in drawing our scene.
+ * @class Sphere
+ */
+class Sphere {
+  constructor (radius, mass, color, position, velocity) {
+    this.radius = radius
+    this.mass = mass
+    this.color = color
+    this.position = position
+    this.velocity = velocity
+  }
+
+  get radius () {
+    
+  }
+}
